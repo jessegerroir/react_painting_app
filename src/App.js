3d15128e00b22e71450d2e847501434d84849d13
JesseGerroir <jessegerroir@gmail.com>
2018-07-12 18:13:27 -0400

    Created intinal board and tiles
    Created intinal board and tiles

--- src/App.js
+++ src/App.js
@@ -2,20 +2,52 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Board boardWidth='800' boardHeight='800'></Board>
       </div>
     );
   }
+}
+
+const Board = ({boardWidth, boardHeight}) => {
+    const totalSquares = (boardWidth /50) *  (boardHeight /50);
+    let tiles = [];
+    for (let i = 0; i < totalSquares; i++) {
+        tiles.push(<Tile></Tile>);
+    }
+    return(
+        <div className='board'>
+            {tiles}
+        </div>
+    )
+}
+
+class Tile extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="tile"></div>
+        );
+    }
+}
+
+class Game extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="tile"></div>
+        );
+    }
 }
 
 export default App;
